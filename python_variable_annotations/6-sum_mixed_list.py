a21b4dc7fa3c25f997f750da5fe44a2bb8fad7ea
Lauro <laurolamas@gmail.com>
2023-08-28 19:50:50 -0300

    Fix_6.1

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(input_list: List[Union[float, int]]) -> float:
+def sum_mixed_list(input_list: List[Union[int, float]]) -> float:
     """Sum mixed list"""
     return sum(input_list)
