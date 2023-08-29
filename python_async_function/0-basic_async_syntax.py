c83ddccb676f062a88cb975fea5edab73bb0ef84
Lauro <laurolamas@gmail.com>
2023-08-28 22:07:22 -0300

    Fix_0.1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Task 0"""
+
+
 import asyncio
 import random
 
 
-async def wait_random(n: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Wait random"""
-    random_float = random.uniform(0.0, n)
+    random_float = random.uniform(0.0, max_delay)
     await asyncio.sleep(random_float)
     return random_float
