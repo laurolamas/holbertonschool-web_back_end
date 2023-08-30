712ede610e2c273fb854118e5b1b9ceca270e66e
Lauro <laurolamas@gmail.com>
2023-08-30 14:10:54 -0300

    Added_0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 0"""
+
+import random
+import time
+
+
+async def async_generator():
+    """Async generator"""
+    for i in range(10):
+        random_number = random.uniform(0, 10)
+        time.sleep(1)
+        yield(random_number)
