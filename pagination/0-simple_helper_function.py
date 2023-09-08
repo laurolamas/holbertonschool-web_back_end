c05ce4c44c5220c17962835105a89fcf22772f33
Lauro <laurolamas@gmail.com>
2023-09-08 17:22:29 -0300

    Added_0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Write a function named index_range that takes two integer arguments page and page_size.
+"""
+
+def index_range(page, page_size):
+    """hola"""
+    return (page_size*(page-1), page_size*page)
