8ac274abd6fe50fd7c38fcab1db50b0615489348
Lauro <laurolamas@gmail.com>
2023-09-08 16:58:26 -0300

    Fix_12.1

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,30 +3,31 @@
 Write a Python script that provides some
 stats about Nginx logs stored in MongoDB:
 """
-import pymongo
+if __name__ == '__main__':
+    import pymongo
 
-# MongoDB connection
-client = pymongo.MongoClient('mongodb://localhost:27017/')
-db = client['logs']
-collection = db['nginx']
+    # MongoDB connection
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    collection = db['nginx']
 
-# Count the total number of documents in the collection
-total_logs = collection.count_documents({})
+    # Count the total number of documents in the collection
+    total_logs = collection.count_documents({})
 
-# Count the number of documents with each HTTP method
-http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {}
-for method in http_methods:
-    method_count = collection.count_documents({"method": method})
-    method_counts[method] = method_count
+    # Count the number of documents with each HTTP method
+    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {}
+    for method in http_methods:
+        method_count = collection.count_documents({"method": method})
+        method_counts[method] = method_count
 
-# Count the number of documents with method=GET and path=/status
-status_check_count = collection.count_documents(
-    {"method": "GET", "path": "/status"})
+    # Count the number of documents with method=GET and path=/status
+    status_check_count = collection.count_documents(
+        {"method": "GET", "path": "/status"})
 
-# Print the statistics
-print(f"{total_logs} logs")
-print("Methods:")
-for method, count in method_counts.items():
-    print(f"    method {method}: {count}")
-print(f"{status_check_count} status check")
+    # Print the statistics
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method, count in method_counts.items():
+        print(f"    method {method}: {count}")
+    print(f"{status_check_count} status check")
