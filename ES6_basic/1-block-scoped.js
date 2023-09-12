3996604a3f68b0a57f7fa131ba20d9d64f74bb7a
Lauro <laurolamas@gmail.com>
2023-09-12 18:57:05 -0300

    Fixx_1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
+    return [false, true];
   }
-
-  return [task, task2];
+  return [false, true];
 }
